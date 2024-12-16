e659b613b2c1093811626fa908f306d2608f5f5f
paulhaa <paulhaa>
2024-12-16 17:00:23 +0100

    Add lsp spec json

---
+++ lspSpec/generateModels.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# This script uses quicktype (https://github.com/glideapps/quicktype) to generate the lsp models.
+
+SOURCE_PATH="schema.json"
+GEN_PATH="../src/server/models/generated"
+
+rm -rf ${GEN_PATH}
+
+mkdir ${GEN_PATH}
+
+quicktype --src-lang schema \
+  ${SOURCE_PATH} \
+  --lang cpp \
+  --namespace lsps \
+  --no-boost \
+  --source-style multi-source \
+  --include-location global-include \
+  --out ${GEN_PATH}/models.hpp
